231f5ae9dd2d3d2bf6ce7818d0928b1dbffc0adc
MickaelRigault <mick.rigault@gmail.com>
2021-01-27 15:01:02 +0100

    latest

---
+++ ztfdr/config.py
@@ -0,0 +1,9 @@
+
+
+
+PECULIARS = {"91bg":["ZTF18aabstmw", "ZTF18aajtlbf", "ZTF18aaykmzg",
+                     "ZTF18abjrznx","ZTF18abltdfj","ZTF18abnzocn", 
+                     "ZTF18acefgoc", "ZTF18aczdapb"],
+             
+             "Iax": ["ZTF18abdmgab"]
+            }
